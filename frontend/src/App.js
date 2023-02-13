35c0d64d87b54d055cf516e1beba8f529327f49b
RandallCaler <71289971+RandallCaler@users.noreply.github.com>
2023-02-13 14:00:57 -0800

    Added base of React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [lng, setLng] = useState(-120.66318847361558);
   const [lat, setLat] = useState(35.303280314081285);
   const [zoom, setZoom] = useState(14);
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -37,6 +38,58 @@ function App() {
       <div ref={mapContainer} className="map-container" />
     </div>
   );
+
+  function removeOneCharacter (_index) {
+    const updated = characters.filter((character, i) => {
+      return i !== _index
+    });
+    const person = characters[_index];
+    const id=person['_id'];
+    const response = axios.delete(`http://localhost:5000/users/${id}`);
+    setCharacters(updated);
+  }
+  /*function updateList(person) {
+    setCharacters([...characters, person]);
+  }*/
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 201)
+       setCharacters([...characters, result.data] );
+    });
+ }
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+ }
+ useEffect(() => {
+  fetchAll().then( result => {
+     if (result)
+        setCharacters(result);
+   });
+}, [] );
+async function makePostCall(person){
+  try {
+     const response = await axios.post('http://localhost:5000/users', person);
+     return response;
+  }
+  catch (error) {
+     console.log(error);
+     return false;
+  }
+}
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
 }
 
 export default App;
