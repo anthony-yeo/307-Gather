c00e231d64fae7451e7275377da02da063012934
Ash <86912625+anarchaworld@users.noreply.github.com>
2023-03-03 13:31:21 -0800

    added ui library

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Button from '@mui/material/Button';
+
 function MainPage() {
   return (
+
     <div className="container">
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
       <BrowserRouter basename="/">
         <nav>
           <ul>
