cc2b41b9c9d5826baf2602671d837dc3d94d9e9b
Anthony Yeo <47506456+anthony-yeo@users.noreply.github.com>
2023-03-12 14:39:07 -0700

    Update userServices.js

--- models/userServices.js
+++ models/userServices.js
@@ -11,153 +11,139 @@ require('dotenv').config();
 const conn_str = 'mongodb+srv://ProjectGather:'+process.env.DB_PASSWORD+'@project-gather.iidopil.mongodb.net/?retryWrites=true&w=majority'
        
 
+
 try {
   // Connect to the MongoDB cluster
    mongoose.connect(
     conn_str,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log('> MONGODB users connection \t- successful')
+    () => console.log("> MONGODB events connection \t- successful")
   );
 
 } catch (e) {
-  console.log('> MONGODB users connection \t- failed');
+  console.log("> MONGODB events connection \t- failed");
 }
 
-//WHAT ARE SOME WAYS WE WANT TO FILTER USER BY?
-//--VERIFIED USERS
-async function getUsers(first, last) {
-  let result;
 
 
+async function getUsers(query) {
   //http://localhost:5000/users/?firstName=test&lastName=test
 
-  if (first === undefined && last === undefined){
-    result = await userModel.find();
+  if (isEmpty(query)){
+    return await userModel.find();
   }
-  else if (last === undefined){
-    result = await userModel.find({firstName:first});
-    //return firstName;
-  } 
-  else if (first === undefined){
-    result = await userModel.find({lastName:last});
+  else if (query.verified !== undefined){
+    return await userModel.find({verified: query.verified});
   }
-  else{
-    result = await userModel.find({firstName:first,lastName:last});
+  else if(query.firstName !== undefined && query.lastName !== undefined){
+    return await userModel.find({firstName:query.firstName,
+                                   lastName:query.lastName});
   }
-  return result; 
+  else if (query.firstName !== undefined){
+    return await userModel.find({firstName:{$regex : query.firstName, $options : 'i'}});
+  }
+
+  else if (query.lastName !== undefined){
+    return await userModel.find({lastName:{$regex : query.lastName, $options : 'i'}});
+  }
+
 }
 
 
 async function findUserById(id) {
-  try {
     return await userModel.findById(id);
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
+}
+
+async function findUserByEmail(email){
+  return await userModel.find({email:email});
 }
 
 async function addUser(user) {
-  try {
-    const checkEmail = await userModel.findOne({email:user.email});
-    
-    if (checkEmail !== undefined || checkEmail === null){
-      const hash = bcrypt.hashSync(user.password, 10);
-      user.password = hash;
+  const checkEmail = await userModel.findOne({email:user.email});
 
-      const userToAdd = new userModel(user);
-      const savedUser = await userToAdd.save();
+  if (checkEmail === null){
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
 
-      return savedUser;
-    }
-    else{
-      return checkEmail;
-    }
-    
-  } catch (error) {
-    console.log(error);
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+
+    return savedUser;
+  }
+  else{
     return undefined;
   }
 }
 
 async function saveEvent(userId, eventId) {
-  try {
-    var savedEvent = await attendanceModel.findOne({event_id:eventId});
-    if (savedEvent === null) return false;
+  var savedEvent = await attendanceModel.findOne({event_id:eventId});
+  if (savedEvent === null) return false;
 
-    var newAttendeeList = savedEvent.attendees;
-    newAttendeeList.push(userId);
+  var newAttendeeList = savedEvent.attendees;
+  newAttendeeList.push(userId);
 
-    await attendanceModel.updateOne({'event_id':eventId}, {
-      attendees: newAttendeeList
-    });
+  await attendanceModel.updateOne({'event_id':eventId}, {
+    attendees: newAttendeeList
+  });
 
-    return savedEvent;
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
+  return savedEvent; 
 }
+
 
 async function validateUser(reqInfo) {
   if (reqInfo.email === undefined || reqInfo.password === undefined) return false;
-  try {
-    const user = await userModel.findOne({ 'email':reqInfo.email });
-    if (user === undefined) return false;
 
-    return bcrypt.compareSync(reqInfo.password, user.password);
-  } catch (error) {
-    console.log(error);
-    return undefined;
-  }
+  const user = await userModel.findOne({ 'email':reqInfo.email });
+  if (user === null) return false;
+
+  return bcrypt.compareSync(reqInfo.password, user.password);
+  
 }
 
 async function addFriend(userId, friendId) {
 
-  try {
-    var user = await userModel.findOne({'_id':userId});
-    var userFriends = user.friends;
-
-    var friend = await userModel.findById({'_id': friendId});
-    var friendFriends = friend.friends;
-
-    if (friend === undefined || user === undefined) return undefined;
-
-    
-    if(userFriends.includes(friendId) || friendFriends.includes(userId)) return false;
-
-    userFriends.push(friend);
-    friendFriends.push(user);
-
-    await userModel.updateOne({ '_id':userId }, {
-      friends: userFriends
-    });
-
-    await userModel.updateOne({'_id':friendId}, {
-      friends: friendFriends
-    });
-
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
+  if (userId === undefined || friendId === undefined){
+    return undefined;
   }
+
+  var user = await userModel.findOne({'_id':userId});
+  var friend = await userModel.findById({'_id': friendId});
+
+  if (friend === null || user === null) return undefined;
+
+  var userFriends = user.friends;
+  var friendFriends = friend.friends;
+  
+  if(userFriends.includes(friendId) || friendFriends.includes(userId)) return false;
+
+  userFriends.push(friend);
+  friendFriends.push(user);
+
+  await userModel.updateOne({ '_id':userId }, {
+    friends: userFriends
+  });
+
+  await userModel.updateOne({'_id':friendId}, {
+    friends: friendFriends
+  });
+
+  return true;
 }
 
 async function delUser(body){
-  try{
-    if (body.userId === undefined){
-      return userModel.find({'email':body.email}).remove();
-    }
-    else if (body.email === undefined){
-      return userModel.find({'_id':body.userId}).remove();
-    }
-    else
-      return false;
-  } catch (error) {
-    console.log(error);
-    return false;
+  if (body.email !== undefined){
+    return userModel.find({'email':body.email}).remove();
   }
+  else if (body.userId !== undefined){
+    return userModel.find({'_id':body.userId}).remove();
+  }
+  else
+    return false;
+}
+
+
+function isEmpty(obj) {
+  return !Object.keys(obj).length > 0;
 }
 
 exports.getUsers = getUsers;
@@ -167,5 +153,4 @@ exports.delUser = delUser;
 exports.validateUser = validateUser;
 exports.saveEvent = saveEvent;
 exports.addFriend = addFriend;
-
-  
+exports.findUserByEmail = findUserByEmail;
